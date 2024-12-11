81d3fe6668b4836cd597da96b1ca088c199bd38a
xlester <humboldt.medina@gmail.com>
2024-12-11 12:06:23 -0600

    add FastAPI module to project, tested on main.py

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,23 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')# ruta desde donde se podra ver la lista
+def get_list():
+    return [1,2,3,4,5,6]
+
+@app.get('/contact')
+def get_list():
+    return {'name':'Orlando'}
+
+@app.get('/web', response_class=HTMLResponse)
+def get_html():
+    return"""
+    <h1>Mi pagina usando FastAPI</h1>
+    <p>Para esto utilice HTMLResponse de FastAPI</p>
+    """
 
 def run():
     store.get_categories()
